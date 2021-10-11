0d79d28d681e27f64c1d0a9d3b88a4b61f462198
jtj3107 <jtj3926@gmail.com>
2021-10-11 18:44:36 +0900

    Rq, 인터셉터 구현

--- src/main/java/com/jtj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/jtj/exam/demo/controller/UsrArticleController.java
@@ -5,8 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.jtj.exam.demo.vo.Rq;
+
 @Controller
 public class UsrArticleController {
+	private Rq rq;
+	
+	public UsrArticleController(Rq rq) {
+		this.rq = rq;
+	}
+	
 	@RequestMapping("/usr/article/list")
 	public String showList(Model model, @RequestParam(defaultValue = "0") int boardId,
 			@RequestParam(defaultValue = "title,body") String searchKeywordTypeCode,
---
+++ src/main/java/com/jtj/exam/demo/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.jtj.exam.demo.service;
+
+import com.jtj.exam.demo.vo.Member;
+
+public class MemberService {
+
+	public Member getMemberById(int loginedMemberId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
